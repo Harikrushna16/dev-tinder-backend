a5b4d53b8ce98cfb8aa7d1ed0b06ddfd439a21a9
Harikrushna16 <patelharikrushan8416@gmail.com>
2026-03-01 13:37:35 +0530

    added new cors URL

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://13.61.12.3:3000/"],
+    origin: ["http://localhost:5173", "https://13.61.12.3:3000/", "https://13.61.12.3/"],
     credentials: true
 }));
 
